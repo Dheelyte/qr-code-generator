149977355d9955b522fdfd34539b2674c9b66dbc
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-05 00:00:03 +0100

    Add gunicorn and allowed hosts

--- qr_code/settings.py
+++ qr_code/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-70%!qvl)*9#byoq7zy=j69uwv=gcx)_rj#e+=y$%bkbnb@wudl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qr-genx.onrender.com']
 
 
 # Application definition
